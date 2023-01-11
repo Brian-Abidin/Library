ba2ebe597a33fa30c9faf8d22a5c71f468d519c0
Brian Abidin <Brian.abidin1@gmail.com>
2023-01-11 17:05:57 -0500

    added mylibrary array, book function with example

--- library.js
+++ library.js
@@ -1,1 +1,25 @@
-   const book
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  // on form submit object.create(book) with the proper properties
+
+  myLibrary.push(book); // add book to myLibrary Array
+}
+
+const book1 = new Book(
+  "Green Eggs and Ham",
+  "Dr.Suess",
+  "50 pages",
+  "not read yet"
+);
+
+addBookToLibrary(book1);
+
+console.log(myLibrary);
